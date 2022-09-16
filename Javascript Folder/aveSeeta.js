e51a0c06aa9bbca010ab0709ef52e65b43345f22
ThrinadhChowdary Gutta <98127965+thrinadh-chows@users.noreply.github.com>
2022-09-16 08:22:53 -0500

    updated

---
+++ Javascript Folder/aveSeeta.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs=require('fs')
+const port = 3000
+const server = http.createServer(function(req,res){
+    
+    res.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile('login1.html',function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('error:file not found')
+        }else{
+            res.write(data)
+        }
+        res.end()
+    })
+   
+})
+
+server.listen(port, function(error){
+if(error){
+    console.log("something went wrong", error)
+}else{
+     console.log("darlings are listening",port )
+}
+})

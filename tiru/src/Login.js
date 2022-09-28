dd2cb8d14e066abd3cd681c791d0127aad13c488
ThrinadhChowdary Gutta <98127965+thrinadh-chows@users.noreply.github.com>
2022-09-28 08:57:56 -0500

    index page updated

--- tiru/src/Login.js
+++ tiru/src/Login.js
@@ -1,102 +1,37 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
-import "./login.css";
-import Regi from "./Regi.js";
-
-function Login() {
-
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
-
-  const handleSubmit1 = () =>{
-      window.location.href = "Regi.js"
-      Regi();
-  }
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
+import { useState } from "react";
+import Dashboard from "./Dashboard";
+const Login = () => {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [authenticated, setauthenticated] = useState(localStorage.getItem(localStorage.getItem("authenticated")|| false));
+  const users = [{ username: "Jane", password: "testpassword" }];
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const account = users.find((user) => user.username === username);
+    if (account && account.password === password) {
+        setauthenticated(true)
+        localStorage.setItem("authenticated", true);
     }
   };
-
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  // JSX code for login form
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>      
-
-      </form>
-      <div class="footee">
-          <button onClick={ () => handleSubmit1()} type="button" class="button-register">Register</button>
-          
-      </div>
-      
-    </div>
-   
-  );
-
   return (
-    <div className="app">
-      <div className="login-form">
-        <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
+    <div>
+      <p>Welcome Back</p>
+      <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="Username"
+        value={username}
+        onChange={(e) => setusername(e.target.value)}
+      />
+      <input
+        type="password"
+        name="Password"
+        onChange={(e) => setpassword(e.target.value)}
+      />
+      <input type="submit" value="Submit" />
+      </form>
     </div>
-  );
-}
+  )
+};
 
 export default Login;
--- tiru/src/index.js
+++ tiru/src/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css'
-import './login.css'
-
-ReactDOM.render(<App />,document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);

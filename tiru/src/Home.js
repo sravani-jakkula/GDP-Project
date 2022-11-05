229af5f4e00d2db6d45fb7ac23686acd2cc102ee
ThrinadhChowdary Gutta <98127965+thrinadh-chows@users.noreply.github.com>
2022-11-04 23:04:33 -0500

    added some components

--- tiru/src/Home.js
+++ tiru/src/Home.js
@@ -4,20 +4,34 @@ import "./Login.css"
 
 export default function home() {
   return (
-   <>
+    <>
+    <div className='body'>
+      <div className='tops'>
+         <img src={Green} className="image"/>
+         <a href="#">home</a>
+         <a href="#">Login</a>
+         <a href="#">Register</a>      
+         <input type="text" id="text" placeholder='search'/>
+         <a href="#">LogOut</a>  
+      </div>
+      <div className='bottoms'>
+ 
+      </div>
+    </div>
+    <>
    <div className='body'>
      <div className='tops'>
         <img src={Green} className="image"/>
-        <a href="#">home</a>
-        <a href="#">Login</a>
-        <a href="#">Register</a>      
-        <input type="text" id="text" placeholder='search'/>
-        <a href="#">LogOut</a>  
+        <a href="#">assets</a>
+        <a href="#">LogOut</a>
+        <a href="#">help</a>      
+        
      </div>
      <div className='bottoms'>
 
      </div>
    </div>
    </>
+    </>
   )
 }

0154319b14944acd641d6bfebb178d2fe9081d45
ThrinadhChowdary Gutta <98127965+thrinadh-chows@users.noreply.github.com>
2022-11-24 14:28:50 -0600

    created some columns in Student dashbaord

--- Student-dashboard.js
+++ Student-dashboard.js
@@ -4,24 +4,14 @@ import { Link } from 'react-router-dom'
 function StudentDash() {
     return (
         
-        <div>
-            <div className="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
-                <div className="app-header header-shadow">
-                    <div className="app-header__logo">
-                        <div className="logo-src" style={{ background: 'none' }} />
-                        <div className="header__pane ml-auto">
-                            <div>
-                                <button type="button" className="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                    <span className="hamburger-box">
-                                        <span className="hamburger-inner" />
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="app-header__mobile-menu">
+    <div>
+        <div className="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+            <div className="app-header header-shadow">
+                <div className="app-header__logo">
+                    <div className="logo-src" style={{ background: 'none' }} />
+                    <div className="header__pane ml-auto">
                         <div>
-                            <button type="button" className="hamburger hamburger--elastic mobile-toggle-nav">
+                            <button type="button" className="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
                                 <span className="hamburger-box">
                                     <span className="hamburger-inner" />
                                 </span>
@@ -29,8 +19,55 @@ function StudentDash() {
                         </div>
                     </div>
                 </div>
+                <div className="app-header__mobile-menu">
+                    <div>
+                        <button type="button" className="hamburger hamburger--elastic mobile-toggle-nav">
+                            <span className="hamburger-box">
+                                <span className="hamburger-inner" />
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div className="app-header__menu">
+                    <span>
+                        <button type="button" className="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                            <span className="btn-icon-wrapper">
+                                <i className="fa fa-ellipsis-v fa-w-6" />
+                            </span>
+                        </button>
+                    </span>
+                </div>
+                <div className="app-header__content">
+                    <div className="app-header-right">
+                        <div className="header-btn-lg pr-0">
+                            <div className="widget-content p-0">
+                                <div className="widget-content-wrapper">
+                                    <div className="widget-content-left">
+                                        <div className="btn-group">
+                                            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" className="p-0 btn">
+                                            <img width={42} className="rounded-circle" src="/Images/auth/user.png" alt="" />
+                                                {/* <i className="fa fa-angle-down ml-2 opacity-8" /> */}
+                                            </a>
+                                            <div tabIndex={-1} role="menu" aria-hidden="true" className="dropdown-menu dropdown-menu-right">
+                                                <Link to="/profile" type="button" tabIndex={0} className="dropdown-item">Profile</Link>
+                                                <div tabIndex={-1} className="dropdown-divider" />
+                                                <button type="button" tabIndex={0} className="dropdown-item">logout</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="widget-content-left  ml-3 header-user-info">
+                                        <div className="widget-heading">
+                                                Faculty Dash
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
     )
  }
  export default StudentDash;
